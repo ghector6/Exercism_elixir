f5ed490830d73699f04abfe75500ca2edaae4777
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-06 22:34:01 -0600

    add: cleaning comments

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -1,21 +1,19 @@
 defmodule FreelancerRates do
+
   def daily_rate(hourly_rate) do
     hourly_rate * (8 / 1)
-    # Please implement the daily_rate/1 function
   end
 
   def apply_discount(before_discount, discount) do
     before_discount - (before_discount * discount / 100)
-    # Please implement the apply_discount/2 function
   end
 
   def monthly_rate(hourly_rate, discount) do
-    # Please implement the monthly_rate/2 function
       ceil(apply_discount(daily_rate(hourly_rate * 22), discount))
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
     budget / apply_discount(daily_rate(hourly_rate), discount) |> Float.floor(1)  
-    # Please implement the days_in_budget/3 function
   end
+
 end
