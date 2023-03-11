83e92524f75ecbde3dd4277250a828aa3c96d566
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-10 19:45:06 -0600

    refactor: cleaning exercise

--- language-list/lib/language_list.ex
+++ language-list/lib/language_list.ex
@@ -1,33 +1,27 @@
 defmodule LanguageList do
+
   def new() do
     []
-    # Please implement the new/0 function
   end
 
   def add(list, language) do
     [language | list ]
-    # Please implement the add/2 function
   end
 
   def remove(list) do
     [head | tail] = list; tail    
-    # Please implement the remove/1 function
   end
 
   def first(list) do
     List.first(list)
-    # Please implement the first/1 function
   end
 
   def count(list) do
     Kernel.length(list)
-    # Please implement the count/1 function
   end
-  
-  
 
   def functional_list?(list) do
     Enum.member?(list, "Elixir")
-    # Please implement the functional_list?/1 function
   end
+
 end
