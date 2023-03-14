1d0f7f56e8b568f90dc8fb10b54e52097da3c56e
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-13 19:17:17 -0600

    add: first solution using cond and not

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,5 +1,14 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
+    cond do
+      level == 0 and not legacy? -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5  and not legacy? -> :fatal
+      true -> :unknown
+    end
     # Please implement the to_label/2 function
   end
 
