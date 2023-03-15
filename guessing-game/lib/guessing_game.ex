4dd4ca390945258345e1dd6a61a5dd62d54f840e
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-14 19:29:52 -0600

    add: second function for high gessing

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,3 +1,5 @@
 defmodule GuessingGame do
   def compare(secret_number, guess) when secret_number == guess, do: "Correct"
+  def compare(secret_number, guess) when secret_number < guess, do: "Too high"
+
 end
