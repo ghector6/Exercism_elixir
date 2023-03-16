5062eeb9e9778ee7dcb1afa4e6148500be7396c7
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-15 21:17:40 -0600

    fixing warnings

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,6 +1,5 @@
 defmodule GuessingGame do
-  def compare(secret_number ), do: "Make a guess"
-  def compare(secret_number, guess) when is_integer(guess) == false, do: "Make a guess"
+  def compare(_secret_number, guess \\ "") when is_integer(guess) == false, do: "Make a guess"
   def compare(secret_number, guess) when secret_number == guess, do: "Correct"
   def compare(secret_number, guess) when secret_number == guess + 1 or secret_number == guess - 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
