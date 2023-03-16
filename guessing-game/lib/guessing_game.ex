6ae6b67ae4af20eeecc06a36704ab4b961a8014f
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-15 20:37:24 -0600

    add: solutions for exercise

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,7 +1,8 @@
 defmodule GuessingGame do
+  def compare(secret_number ), do: "Make a guess"
+  def compare(secret_number, guess) when is_integer(guess) == false, do: "Make a guess"
   def compare(secret_number, guess) when secret_number == guess, do: "Correct"
   def compare(secret_number, guess) when secret_number == guess + 1 or secret_number == guess - 1, do: "So close"
-
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
   def compare(secret_number, guess) when secret_number > guess, do: "Too low"
 end
