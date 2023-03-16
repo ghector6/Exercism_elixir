5ebdc27e32d674123431b76891f6de3698696174
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-15 21:35:10 -0600

    add: header with default values

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,5 +1,6 @@
 defmodule GuessingGame do
-  def compare(_secret_number, guess \\ "") when is_integer(guess) == false, do: "Make a guess"
+  def compare(_secret_number, guess \\ "")
+  def compare(_secret_number, guess) when is_integer(guess) == false, do: "Make a guess"
   def compare(secret_number, guess) when secret_number == guess, do: "Correct"
   def compare(secret_number, guess) when secret_number == guess + 1 or secret_number == guess - 1, do: "So close"
   def compare(secret_number, guess) when secret_number < guess, do: "Too high"
