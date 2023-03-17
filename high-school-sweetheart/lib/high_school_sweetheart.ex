5ff63149f59e0ab02ea94f63e2a853eb044ccf69
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-16 22:03:34 -0600

    cleaning

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -3,20 +3,17 @@ defmodule HighSchoolSweetheart do
     name
     |> String.trim()
     |> String.first()
-    # Please implement the first_letter/1 function
   end
 
   def initial(name) do
     first_letter(name)
     |> String.upcase()
     |> Kernel.<>(".")
-    # Please implement the initial/1 function
   end
 
   def initials(full_name) do
     [a,b] = String.split(full_name) 
     Kernel.<>(initial(a), " #{initial(b)}")  
-    # Please implement the initials/1 function
   end
 
   def pair(full_name1, full_name2) do
@@ -36,7 +33,5 @@ defmodule HighSchoolSweetheart do
                  ***
                   *
     """      
-
-    # Please implement the pair/2 function
   end
 end
