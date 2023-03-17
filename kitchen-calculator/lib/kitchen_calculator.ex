8a639ada64f7e21b812bdeabc2d56223d8c21aa6
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-16 18:25:08 -0600

    add: third functions from milliliter to other unit

--- kitchen-calculator/lib/kitchen_calculator.ex
+++ kitchen-calculator/lib/kitchen_calculator.ex
@@ -12,8 +12,24 @@ defmodule KitchenCalculator do
   def to_milliliter({:tablespoon, measure}), do: {:milliliter, measure * 15}
 
 
-  def from_milliliter(volume_pair, unit) do
-    # Please implement the from_milliliter/2 functions
+  def from_milliliter({:milliliter, measure}, :cup) do
+    {:cup, measure / 240}
+  end
+
+  def from_milliliter({:milliliter, measure}, :fluid_ounce) do
+    {:fluid_ounce, measure / 30}
+  end
+
+  def from_milliliter({:milliliter, measure}, :teaspoon) do
+    {:teaspoon, measure / 5}
+  end
+
+  def from_milliliter({:milliliter, measure}, :tablespoon) do
+    {:tablespoon, measure / 15}
+  end
+
+  def from_milliliter({:milliliter, measure}, :milliliter) do
+    {:milliliter, measure}
   end
 
   def convert(volume_pair, unit) do
