dbb6cc21f8af0afa8dce9a1b504f98f9adc8efa1
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-20 17:38:19 -0600

    add: last set of functions for last instructions

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -12,7 +12,15 @@ defmodule BirdCount do
   def total([]), do: 0
   def total([head| tail]), do: head + total(tail)
 
-  def busy_days(list) do
+  def busy_days([]) do
+    0
     # Please implement the busy_days/1 function
   end
+  def busy_days([head | tail]) when head >=5  do
+    1 + busy_days(tail)
+  end
+  def busy_days([head | tail]) do
+    busy_days(tail)
+  end
+
 end
