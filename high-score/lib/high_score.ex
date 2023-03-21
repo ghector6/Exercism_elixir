dd143cbcbd66e2ffa6b9a92657f2d5f19f0d81b2
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-21 09:41:15 -0600

    refactor and beautyfiyng

--- high-score/lib/high_score.ex
+++ high-score/lib/high_score.ex
@@ -1,32 +1,18 @@
 defmodule HighScore do
   @initial_score 0
-  def new() do
-    %{}
-    # Please implement the new/0 function
-  end
+  def new(), do: %{}
 
   def add_player(scores, name, score \\ @initial_score) do
     Map.put(scores, name, score)
-    # Please implement the add_player/3 function
   end
 
-  def remove_player(scores, name) do
-    Map.delete(scores,name)
-    # Please implement the remove_player/2 function
-  end
+  def remove_player(scores, name), do: Map.delete(scores,name)
 
-  def reset_score(scores, name) do
-    add_player(scores,name )
-    # Please implement the reset_score/2 function
-  end
+  def reset_score(scores, name), do: add_player(scores,name )
 
   def update_score(scores, name, score) do
     Map.update(scores, name, score, fn value -> value + score end )
-    # Please implement the update_score/3 function
   end
 
-  def get_players(scores) do
-    Map.keys(scores)
-    # Please implement the get_players/1 function
-  end
+  def get_players(scores), do: Map.keys(scores)
 end
