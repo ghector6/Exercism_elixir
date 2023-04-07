78d831ce668c4764e8b8d09a9cb81f2d51995a8b
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-07 16:18:58 -0600

    add: recursion solution for first exercise
    adding fn head and fn tail recursion solution
    🤓

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,5 +1,17 @@
 defmodule Username do
-  def sanitize(username) do
+  def sanitize( username,  acc \\ [])
+  def sanitize([], acc), do: Enum.reverse(acc)
+  def sanitize([h | t] = username, acc) do
+    case h do
+      h when h in ?a..?z -> sanitize(t,[h|acc])
+      _ -> sanitize(t, acc)
+    end
+
+
+
+
+#    Enum.filter(username, fn x -> x >= 97 and x <= 122 end)
+
     # ä becomes ae
     # ö becomes oe
     # ü becomes ue
