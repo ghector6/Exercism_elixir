1f5e6d8e34d65a06edc89672e99a891b05c62ed0
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-07 19:30:45 -0600

    add: conditions for under score still missing a test

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,8 +1,12 @@
 defmodule Username do
   def sanitize( username,  acc \\ [])
   def sanitize([], acc), do: Enum.reverse(acc)
-  def sanitize([h | t] = username, acc) do
+  def sanitize([h | t] = _username, acc) do
     case h do
+       228 -> sanitize(t, [?a, ?e| acc])
+       246 -> sanitize(t, [?o, ?e| acc])
+       252 -> sanitize(t, [?u, ?e| acc])
+       223 -> sanitize(t, [?s, ?s| acc])
       h when h in ?a..?z or h == 95 -> sanitize(t,[h|acc])
       _ -> sanitize(t, acc)
     end
