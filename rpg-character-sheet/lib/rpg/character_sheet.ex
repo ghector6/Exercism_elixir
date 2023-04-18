8f8502ce6ecf23db6277bbfc8a040ab33900fa47
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-18 16:02:55 -0600

    cleaning

--- rpg-character-sheet/lib/rpg/character_sheet.ex
+++ rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,26 +1,22 @@
 defmodule RPG.CharacterSheet do
   def welcome() do
     IO.puts "Welcome! Let's fill out your character sheet together."
-    # Please implement the welcome/0 function
   end
 
   def ask_name() do
     response = IO.gets("What is your character's name?\n")
     String.trim(response)
-    # Please implement the ask_name/0 function
   end
 
   def ask_class() do
     response = IO.gets("What is your character's class?\n")
     String.trim(response)
-    # Please implement the ask_class/0 function
   end
 
   def ask_level() do
       IO.gets("What is your character's level?\n")
       |> String.trim()
       |> String.to_integer
-    # Please implement the ask_level/0 function
   end
 
   def run() do
@@ -31,8 +27,5 @@ defmodule RPG.CharacterSheet do
     player = %{class: class, level: level, name: name}
     IO.puts "Your character: #{inspect player}"
     player
-
-
-    # Please implement the run/0 function
   end
 end
