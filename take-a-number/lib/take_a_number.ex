ba62a301baa6bf22013a1e5e6e21a0230567cc36
Hector Garcia Rosales <ghectormus@gmail.com>
2023-10-02 12:02:47 -0600

    practicing with programming elixir processes part

--- take-a-number/lib/take_a_number.ex
+++ take-a-number/lib/take_a_number.ex
@@ -1,6 +1,9 @@
 defmodule TakeANumber do
   def start() do
     pid = spawn(fn -> 0 end)
+    receive do
+      {:report_state, process} -> send(pid, 0)
+    end
         # Please implement the start/0 function
   end
 end
