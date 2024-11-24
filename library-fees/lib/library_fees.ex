20c666a0d3a389189f64d431192763737b78f3df
Hector Garcia Rosales <ghectormus@gmail.com>
2024-11-23 20:35:50 -0600

    ops(test2) ADD: beforenoon fn with if condition

--- library-fees/lib/library_fees.ex
+++ library-fees/lib/library_fees.ex
@@ -2,12 +2,9 @@ defmodule LibraryFees do
   def datetime_from_string(string) do
     {:ok, n_dtime } = NaiveDateTime.from_iso8601(string)
     n_dtime
-    # Please implement the datetime_from_string/1 function
   end
 
-  def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
-  end
+  def before_noon?(datetime), do: if datetime.hour < 12, do: true, else: false
 
   def return_date(checkout_datetime) do
     # Please implement the return_date/1 function
